ad378fd49049cae1fc10faa69624b1720c46ab90
Ali owais <aliowais8028514@gmail.com>
2024-10-18 13:03:48 +0500

    Add initial project files

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Products from '@/components/Products';
+import Sale from '@/components/Sale';
+import Footer from '@/components/Footer';
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Products/>
+      <Sale/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default Home;
